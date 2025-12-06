1c1d64669d20ede7b682d5dc2caee253269c863a
Ben Do <bdo.appworkshop@gmail.com>
2025-12-06 16:48:50 -0600

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Ben Do on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
