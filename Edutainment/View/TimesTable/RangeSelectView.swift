a7631e6cca5a43a5e5b0b3fb23fd6fe6d51b81a3
Ben Do <bdo.appworkshop@gmail.com>
2025-12-07 15:28:41 -0600

    Added new image asset for app icon.

---
+++ Edutainment/View/TimesTable/RangeSelectView.swift
@@ -0,0 +1,18 @@
+//
+//  RangeSelectView.swift
+//  Edutainment
+//
+//  Created by Ben Do on 12/6/25.
+//
+
+import SwiftUI
+
+struct RangeSelectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RangeSelectView()
+}
