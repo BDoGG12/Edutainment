c53c47de9eb27219a2155df5145ef3640886616d
Ben Do <bdo.appworkshop@gmail.com>
2025-12-06 20:00:49 -0600

    Added Player struct model.

---
+++ Edutainment/Model/TimeTableModel.swift
@@ -0,0 +1,14 @@
+//
+//  TimeTableModel.swift
+//  Edutainment
+//
+//  Created by Ben Do on 12/6/25.
+//
+
+import Foundation
+
+struct Player {
+    var selectedNumber: Int
+    var numOfQuestions: Int
+    var playerScore: Int
+}
