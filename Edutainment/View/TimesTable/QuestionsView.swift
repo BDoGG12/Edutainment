fbf5195d228e524fe2c98d8678a803d26a942f0b
Ben Do <bdo.appworkshop@gmail.com>
2025-12-07 10:15:37 -0600

    Inserted NavigationStack for the QuestionsView. Edited incrementQuestionNumber to conditionally continue the game or end it.

--- Edutainment/View/TimesTable/QuestionsView.swift
+++ Edutainment/View/TimesTable/QuestionsView.swift
@@ -15,37 +15,51 @@ struct QuestionsView: View {
     @State private var isCorrect: Bool = false
     @State private var nextPressed: Bool = false
     @State private var gameOver: Bool = false
+    @State private var buttonTitle: String = "Next"
+    @State private var isGoingHome: Bool = false
     
     var body: some View {
-        VStack {
-            // Label for Questions View
-            Text("\(player?.selectedNumber ?? 2) x \(questionNumber) = ?")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            TextField("Enter your answer here...", text: $answer)
-                .textFieldStyle(.roundedBorder)
-                .padding()
-                .keyboardType(.numberPad)
-            // Submit to go to the next question until the last question
-            Button("Next") {
-                print(answer)
-                checkAnswer()
-                
-            }
-            .disabled(answer.isEmpty)
-            .alert(isPresented: $nextPressed) {
-                if isCorrect {
-                    return Alert(title: Text("Correct!"), message: Text("Keep going!"))
-                } else {
-                    return Alert(title: Text("Oops!"), message: Text("Try again!"))
+        NavigationStack {
+            VStack {
+                // Label for Questions View
+                Text("\(player?.selectedNumber ?? 2) x \(questionNumber) = ?")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                TextField("Enter your answer here...", text: $answer)
+                    .textFieldStyle(.roundedBorder)
+                    .padding()
+                    .keyboardType(.numberPad)
+                // Submit to go to the next question until the last question
+                Button(buttonTitle) {
+                    if (buttonTitle == "Finish") {
+                        TimesTableSelectView()
+                    } else {
+                        print(answer)
+                        checkAnswer()
+                    }
+                }
+                .disabled(answer.isEmpty)
+                .alert(isPresented: $nextPressed) {
+                    if (isCorrect && !gameOver) {
+                        return Alert(title: Text("Correct!"), message: Text("Keep going!"))
+                    } else if (!gameOver && !isCorrect) {
+                        return Alert(title: Text("Oops!"), message: Text("Try again!"))
+                    } else {
+                        return Alert(title: Text("Game Over!"), message: Text("Your score is \(score)/\(player?.numOfQuestions ?? 0)"), dismissButton: .destructive(Text("Ok"), action: {
+                            isGoingHome = true
+                            
+                        }))
+                    }
                 }
             }
         }
+        
     }
     
     func incrementQuestionNumber() {
-        if (questionNumber <= player?.numOfQuestions ?? 0) {
+        if (questionNumber == player?.numOfQuestions ?? 0) {
             gameOver = true
+            buttonTitle = "Finish"
         } else {
             questionNumber += 1
             gameOver = false
@@ -61,6 +75,7 @@ struct QuestionsView: View {
         } else {
             isCorrect = false
         }
+        incrementQuestionNumber()
         nextPressed = true
     }
 }
